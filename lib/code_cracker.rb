5ddb11a47d162386138020422d9500662f139c3e
Johan Watelet <johanwatelet@gmail.com>
2023-06-02 22:31:38 +0200

    knuth algorithm

--- lib/code_cracker.rb
+++ lib/code_cracker.rb
@@ -7,36 +7,75 @@ class CodeCracker
     @main_rule_engine = hash[:rule_engine]
     @remaining_guesses = hash[:remaining_guesses]
     @all_codes = generate_all_codes
+    @knuth_codes = @all_codes
+    @possible_codes = @all_codes
     @display = Dislay.new
   end
 
   def crack
-    until @remaining_guesses.negative?
+    is_cracked = false
+
+    until is_cracked
       @display.puts_steps(@remaining_guesses)
-      guess = generate_guess
-      puts guess
-      hint = @main_rule_engine.check(guess)
+      code = get_code
+      puts code
+      hint = @main_rule_engine.check(code)
       puts hint
-      return guess if hint.win?
-
-      rule_engine_with_guess = RuleEngine.new(code: guess)
-
-      @all_codes.select! do |code|
-        guess_check = rule_engine_with_guess.check(code)
-        guess_check == hint
+      if hint.win?
+        is_cracked = true
+      else
+        prune_list(code, hint)
       end
       @remaining_guesses -= 1
     end
   end
 
-  def generate_guess
-    case @remaining_guesses
-    when 12 then Code.new(code: [Peg::RED, Peg::RED, Peg::BLUE, Peg::BLUE])
-    when 11 then Code.new(code: [Peg::YELLOW, Peg::YELLOW, Peg::GREEN, Peg::GREEN])
-    when 10 then Code.new(code: [Peg::WHITE, Peg::WHITE, Peg::PINK, Peg::PINK])
-    else
-      @all_codes.sample
+  def minimax
+    times_found = Hash.new(0)
+    scores = {}
+    @possible_codes.each do |code|
+      @knuth_codes.each do |code_to_crack|
+        score = RuleEngine.new(code: code_to_crack).check(code)
+        times_found[score.to_s] += 1
+      end
+      maximum = times_found.values.max
+      scores[code.to_s] = maximum
     end
+
+    minimum = scores.values.max
+
+    @possible_codes.select do |possible_code|
+      scores[possible_code.to_s] == minimum
+    end
+  end
+
+  def prune_list(last_guess, feedback)
+    @knuth_codes.select! do |code|
+      retrieved_feedback = RuleEngine.new(code: code).check(last_guess)
+      retrieved_feedback.to_s == feedback.to_s
+    end
+  end
+
+  def get_code
+    case @remaining_guesses
+    when 12
+      Code.new(color_string: 'rbgp')
+    else
+      guess_codes = minimax
+      code = get_guess_code_from_list(@knuth_codes, guess_codes)
+      @possible_codes.reject! do |possible_code|
+        possible_code.to_s == code.to_s
+      end
+      code
+    end
+  end
+
+  def get_guess_code_from_list(knuth_codes, guess_codes)
+    knuth_codes.each do |code|
+      guess = guess_codes.find { |guess_code| guess_code.to_s == code.to_s }
+      return guess unless guess.nil?
+    end
+    guess_codes.first
   end
 
   def generate_all_codes
